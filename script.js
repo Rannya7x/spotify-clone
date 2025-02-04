6b05f7a0eb337e5b040ffb3f0d9c81af0aeb518b
Rannya7x <rannya7x@gmail.com>
2025-02-04 23:08:42 +0000

    Added search feature

---
+++ script.js
@@ -0,0 +1,41 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+
+/*CONTROLLER*/
+document.addEventListener('input', () => {
+    const searchTerm = document.getElementById('search-input').value.toLowerCase();
+
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return;
+    };
+
+    requestApi(searchTerm);
+});
+
+/*DATA MODEL*/
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            displayResults(result);
+        })
+        .catch(err => console.log(err));
+}
+
+/*VIEW*/
+function displayResults(result) {
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(artist => {
+        artistName.innerText = artist.name;
+        artistImage.src = artist.urlImg;
+    });
+    resultArtist.classList.remove('hidden'); 
+}
