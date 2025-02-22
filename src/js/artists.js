646f331c2da4150e538b2053d5d5cd1b0b4d333a
Rannya7x <rannya7x@gmail.com>
2025-02-22 18:17:43 +0000

    feat: organize artists data handling into artists.js

---
+++ src/js/artists.js
@@ -0,0 +1,33 @@
+export const fetchArtists = async () => {
+    try{
+        const response = await fetch('../../api-artists/artists.json');
+        const data = await response.json();
+        renderArtists(data.artists);
+    } catch (error) {
+        console.error('Error: ', error);
+    }
+};
+
+export const renderArtists = (artists) => {
+    const containerRow = document.querySelector('.container__row');
+    containerRow.innerHTML = '';
+
+    artists.forEach(artist => {
+        const artistHTML = `
+            <div class="row-item">
+                <div class="img-content">
+                    <img class="row-img" src="${artist.urlImg}" alt="${artist.name}">
+                    <button class="play">
+                        <img src="./src/assets/icons/play-solid.svg" alt="botão de play" class="play-icon">
+                    </button>
+                </div>
+                <div class="row-info">
+                    <span class="text name">${artist.name}</span>
+                    <span class="text category">${artist.genre}</span>
+                </div>
+            </div>
+        `;
+
+        containerRow.insertAdjacentHTML('beforeend', artistHTML);
+    });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import { initSearch } from './search.js'; 
 import { initScrollShadow } from './scroll-effects.js'; 
+import { fetchArtists } from './artists.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initSearch();
   initScrollShadow();
+  fetchArtists();
 });
