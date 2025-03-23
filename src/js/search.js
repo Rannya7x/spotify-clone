a2f5b1f027e114fcfb0ae55db75acb362021cc20
Rannya7x <rannya7x@gmail.com>
2025-03-23 14:23:11 +0000

    Refactor search feature

--- src/js/search.js
+++ src/js/search.js
@@ -1,7 +1,8 @@
 const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById('result-artist');
+const resultArtist = document.getElementById('search-result');
 const resultPlaylist = document.getElementById('result-playlists');
 const mainContent = document.getElementById('main-container');
+const playlistContainer = document.getElementById('playlist-container');
 
 /*DATA MODEL*/
 function requestApi(searchTerm) {
@@ -10,7 +11,6 @@ function requestApi(searchTerm) {
         .then(response => response.json())
         .then(result => {
             displayResults(result);
-            console.log(result);
         })
         .catch(err => console.log(err));
 }
@@ -33,12 +33,33 @@ export const initSearch = () => {
 /*VIEW*/
 function displayResults(result) {
     mainContent.classList.add('hidden');
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+    resultPlaylist.classList.add('hidden');
+    playlistContainer.classList.remove('hidden');
+
+
+    /*const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');*/
+
+    resultArtist.innerHTML = '';
 
     result.forEach(artist => {
-        artistName.innerText = artist.name;
-        artistImage.src = artist.urlImg;
+        const resultHTML = `
+            <div class="artist-card" id="">
+                        <div class="card-img">
+                            <img src="${artist.urlImg}" alt="" id="artist-img" class="artist-img"></img>
+                            <div class="play">
+                                <span class="fa fa-solid fa-play"></span>
+                            </div>
+                        </div>
+                        <div class="card-text">
+                            <a title="${artist.name}" class="vst" href="/">${artist.name}</a>                       
+                            <span class="artist-name" id="artist-name"></span>
+                            <span class="artist-category">${artist.genre}</span>
+                        </div>
+                    </div>
+        `;
+
+        resultArtist.insertAdjacentHTML('beforeend', resultHTML);
     });
     resultArtist.classList.remove('hidden'); 
 }
